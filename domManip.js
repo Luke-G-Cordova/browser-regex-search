88e8e9345a26ab16155485fc2a8d7460038d9adc
Luke Cordova <lukethegreat000@gmail.com>
2021-06-13 01:20:40 -0700

    algorithm seems to work, it seems like there may be a bug when a text node has sibling nodes

--- domManip.js
+++ domManip.js
@@ -32,67 +32,65 @@ function recursive(node, searchText){
     }else {
         // has to be textContent and not innerText because we havent gotten the parent Node yet
         var stg = node.textContent + '';
-        var reg = new RegExp(searchText, 'i');
+        var reg = new RegExp(searchText, 'i');//DO NOT CHANGE TO GLOBAL!!!
         if(reg.test(stg)){
 
             // declare the text Nodes parent
             var parent = node.parentNode;
+            
+            // place holder for the parent node
             var pHoldParent = document.createElement(parent.tagName);
-            if(parent.hasChildNodes()&&parent.childNodes.length>1){
-                pHoldParent.innerHTML = parent.innerHTML;
-                console.log(pHoldParent.innerHTML);
-
+            var pChildren = pHoldParent.childNodes;
+            pHoldParent.innerHTML = parent.innerHTML;
+            var newHtml;
+            if(pChildren&&pChildren.length>1){
+                for(child in pChildren){
+                    if(pChildren[child].nodeType === Node.TEXT_NODE){
+                        pHoldParent.replaceChild(document.createTextNode(createNewInnerText(pChildren[child].data, reg)), pChildren[child]);
+                    }
+                }
             }else {
-                
-                pHoldParent.innerHTML = parent.innerHTML;
-    
-                pHoldParent.innerText = pHoldParent.innerText.replace(reg, `<font class="highlight-me">${reg.exec(parent.innerText)[0]}</font>`);
-                var newHtml = pHoldParent.innerHTML;
-                var left = /&lt;font class="highlight-me"&gt;/g;
-                var right = /&lt;[/]font&gt;/g;
-                newHtml = newHtml.replace(left, `<font class="highlight-me">`); newHtml = newHtml.replace(right, '</font>');
-                
-                parent.innerHTML = newHtml;
+                // replace the holder innerText with the newly constructed string
+                pHoldParent.innerText = createNewInnerText(pHoldParent.innerText, reg);
             }
+            newHtml = pHoldParent.innerHTML;
+            // replace all of my tags that are converted to strings back to tags
+            var left = /&lt;font class="highlight-me"&gt;/g;
+            var right = /&lt;[/]font&gt;/g;
+            newHtml = newHtml.replace(left, `<font class="highlight-me">`); newHtml = newHtml.replace(right, '</font>');
             
-            
-
-            // create a place holder element
-            // var docElem = document.createElement(parent.tagName);
-            // assign the parents html to the place holder
-            // docElem.innerHTML = parent.innerHTML;
-
-            // var manipText = docElem.innerText;
-            // var destroyText = docElem.innerText;
-            
-            
-            // while(reg.test(destroyText)){
-            //     if(replace = reg.exec(destroyText)){
-            //         destroyText = destroyText.replace(reg, '');
-            //         manipText = manipText.replace(reg, `<font style="background-color: yellow">${replace[0]}</font>`);
-            //         console.log(replace[0]);
-            //     }
-            // }
-            
-            // docElem.innerText = manipText;
-
-            // change the innerText of the place holder text to a highlight of the search text
-            // docElem.innerText = docElem.innerText.replace(reg, `<span style="background-color: yellow">${searchText}</span>`);
-            
-
-            // convert the place holder innerHTML into a string
-            // var newHtml = docElem.innerHTML + '';
-
-            // change all &lt; and &gt; with in the string to < and > because innerText auto changes them
-            // newHtml = newHtml.replace(/&lt;/g, '<');newHtml = newHtml.replace(/&gt;/g, '>');
-
-            // give parent innerHTML the new string
-            // parent.innerHTML = newHtml;
-
+            // replace the parent html with newly styled html string
+            parent.innerHTML = newHtml;
         }
-        
     }
     return null;
+}
+function createNewInnerText(myInnerText, reg){
+    var regG = new RegExp(reg, 'ig');
+    // get the matched strings and their indexes inside the parent string
+    var matches = indexesOf(regG, myInnerText);
+    // a constructor variable for the innerText
+    var newInnerText = '';
+    for(var i = 0;i<matches.length;i++){
+        var addString = `<font class="highlight-me">${matches[i][0]}</font>`;
+        // get the string before the match and add it to the constructor
+        newInnerText += 
+            myInnerText.substring(
+                matches[i-1]?matches[i-1][0].length + matches[i-1][1]: 0, 
+                matches[i][1]
+            )
+        ;
+        // add the matched string with styles to the constructor
+        newInnerText += addString;
+        // add the string after the match to the constructor
+        newInnerText += matches[i+1] ? '' : 
+            myInnerText.substring(
+                matches[i][1] + matches[i][0].length, 
+                myInnerText.length
+            )
+        ;
+    }
+    return newInnerText;
 }
 function indexesOf(regExpression, stg){
     var arr = [];
