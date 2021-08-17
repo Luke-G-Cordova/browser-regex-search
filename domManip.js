c289c6204ba365465bbc5f8a1a25f1494229a44a
Luke Cordova <lukethegreat000@gmail.com>
2021-08-17 15:53:44 -0700

    It is working very well! just did some code cleanup

--- domManip.js
+++ domManip.js
@@ -5,10 +5,8 @@ var elemKeys = [];
 chrome.runtime.onMessage.addListener((msg, sender, response) => {
     if((msg.from === 'popup') && (msg.subject === 'newDomInfo')){
         if(elemKeys.indexOf(msg.key) === -1) elemKeys.push(msg.key);
-        
         clearHighlight(msg.key);
-
-        if(msg.data !== ''){
+        if(msg.data !== '' && msg.data[msg.data.length - 1] !== '\\'){
             highlight(document.body, new RegExp(msg.data, 'ig'), function(match){
                 index++;
                 var span = document.createElement("span");
@@ -49,14 +47,6 @@ function clearHighlight(keys){
     }
     index = 0;
 }
-
-// highlight(document.body, new RegExp('the', 'ig'), function(match) {
-//     var span = document.createElement("span");
-//     span.style.backgroundColor = `yellow`;
-//     span.style.color = `black`;
-//     span.textContent = match;
-//     return span;
-// });
 
 function highlight(root, regex, callback, excludes){
     excludes || (excludes = ['script', 'style', 'iframe', 'canvas']);
@@ -106,7 +96,6 @@ function highlight(root, regex, callback, excludes){
         }
     }
 
-    // group the nodes by common parent in an array
     var nodes = [];
     var groupedNodes = [];
     var lastElem;
@@ -127,8 +116,6 @@ function highlight(root, regex, callback, excludes){
         nodes.push(tw.currentNode);
     }
 
-    // console.log(groupedNodes);
-
     var masterStr = '';
     var test;
     var test2;
@@ -138,11 +125,8 @@ function highlight(root, regex, callback, excludes){
 
         masterStr = groupedNodes[i].map(elem => elem.data).join('');
 
-        // for all node groups that contain at least one match
         while(test = regex.exec(masterStr)){
             var lastRegIndex = regex.lastIndex;
-
-            // find nodes with an occurence of the match
             var j = 0;
             var nodeParts = '' + groupedNodes[i][j].data;
 
@@ -150,16 +134,12 @@ function highlight(root, regex, callback, excludes){
                 j++;
                 nodeParts += groupedNodes[i][j].data;
             }
-            // find the match with in the node
+
             regex.lastIndex = 0;
             test2 = regex.exec(groupedNodes[i][j].data);
-            // find if this node only contains part of the match
             var inThisNode = nodeParts.substr(test.index);
 
 
-            // if the current node only contains part of the match,
-            // test2 will not be able to detect, so this is the construction
-            // of test2 to account for that case. 
             test2 || (
                 test2 = [], 
                 test2[0] = inThisNode, 
@@ -209,151 +189,7 @@ function highlight(root, regex, callback, excludes){
 
             nodeParts = '';
             regex.lastIndex = lastRegIndex;
-
         }
         regex.lastIndex = 0;
-        
     }
 }
-// highlight(document.body, new RegExp('the', 'ig'), function(match){
-    //     var span = document.createElement("span");
-    //     span.className = `chrome-regeggz-span highlight-me`;
-    //     span.style.backgroundColor = `yellow`;
-    //     span.style.color = `black`;
-    //     span.textContent = match;
-    //     return span;
-    // });
-    
-    
-    // function highlight(root, regex, callback, excludes){
-    //     excludes || (excludes = ['script', 'style', 'iframe', 'canvas']);
-    //     // TODO: make a filter for the treewalker to filter 
-    //     // out blank text nodes and take out the current filter
-    //     // system in the main loop of this function
-    //     var tw = document.createTreeWalker(root, NodeFilter.SHOW_TEXT);
-    
-    //     var currentNode = tw.currentNode;
-    
-    //     function showFutureNode(){
-    //         var node = tw.nextNode();
-    //         if(!node) return node;
-    //         var prevNode;
-    //         while(excludes.indexOf(node.parentNode.tagName.toLowerCase()) > -1){
-    //             node = tw.nextNode();
-    //         }
-    //         tw.previousNode();
-    //         while(excludes.indexOf(tw.currentNode.parentNode.tagName.toLowerCase()) > -1){
-    //             tw.previousNode();
-    //         }
-    //         return node;
-    //     }
-    //     function changeTWNextNode(){
-    //         tw.nextNode();
-    //         while(excludes.indexOf(tw.currentNode.parentNode.tagName.toLowerCase()) > -1){
-    //             tw.nextNode();
-    //         }
-    //     }
-    //     function trimBadHtmlNodes(node){
-    //         if(node.data.indexOf('\n') !== -1){
-    //             var block = node.nextSibling;
-    //             if(block && block !== root){
-    //                 if(block.nodeType === Node.ELEMENT_NODE && window.getComputedStyle(block, null).display !== 'block'){
-    //                     var before = after = '';
-    //                     after = (node.data[node.data.length - 1] === ' ' || node.data[node.data.length - 1] === '\n') ? ' ' : '';
-    //                     before = (node.data[0] === ' ' || node.data[0] === '\n') ? ' ' : '';
-    //                     node.data = before + node.data.trim() + after;
-    //                 }else if(block.nodeType === Node.ELEMENT_NODE && window.getComputedStyle(block, null).display === 'block'){
-    //                     node.data = ' ' + node.data.trimStart();
-    //                 }
-    //             }
-    //         }
-    //     }
-        
-    //     var nextNode;
-    //     var str = '';
-    //     var test;
-    //     var tag;
-    //     while(
-    //         (currentNode = tw.nextNode())
-    //     ){
-    //         while(
-    //             currentNode &&
-    //             (excludes.indexOf(currentNode.parentNode.tagName.toLowerCase()) > -1 || 
-    //             currentNode.data.trim() === '')
-    //         ){
-    //             currentNode = tw.nextNode();
-    //         }
-    //         if(currentNode){
-    
-    //             trimBadHtmlNodes(currentNode);
-    
-    //             // this statement takes care of all
-    //             // the matches that happen accross
-    //             // single nodes 
-    //             if(test = regex.exec(currentNode.data)){
-    //                 var newNode = currentNode.splitText(test.index);
-    //                 newNode.data = newNode.data.substr(test[0].length);
-    //                 tag = callback(test[0]);
-    //                 newNode.parentNode.insertBefore(tag, newNode);
-    //                 changeTWNextNode();
-    //                 currentNode = tw.currentNode;
-    //             }
-                
-    //             regex.lastIndex = 0;
-                
-    //             // this statement takes care of all 
-    //             // the matches that happen accross
-    //             // multiple nodes
-    //             // ----------------------------------------- TODO -------------------------------------
-    //             // right now it works across only 
-    //             // 2 individual nodes, I need to make 
-    //             // it work accross any amount of individual
-    //             // SIBLING nodes.
-    //             if(nextNode = showFutureNode()){
-    
-    
-    
-    
-    //                 trimBadHtmlNodes(nextNode);
-    
-    
-    //                 var firstHalf = currentNode.data;
-    //                 var lastIndex = 0;
-    //                 while(test = regex.exec(firstHalf)){
-    //                     lastIndex = regex.lastIndex;
-    //                 }
-    //                 regex.lastIndex = lastIndex;
-                    
-    //                 var secondHalf = nextNode.data;
-                    
-    //                 str = firstHalf + secondHalf;
-    //                 test = regex.exec(str);
-    //                 if(test && test.index < firstHalf.length){
-    
-    
-    
-    //                     var newNode = currentNode.splitText(test.index);
-    //                     var ogNewNodeData = newNode.data;
-    //                     newNode.data = '';
-    //                     tag = callback(ogNewNodeData);
-    //                     newNode.parentNode.insertBefore(tag, newNode);
-    //                     currentNode = tw.nextNode();
-    
-    //                     var of = test[0].length - ogNewNodeData.length;
-    //                     var newNextNode = nextNode.splitText(of);
-    //                     changeTWNextNode();
-    //                     currentNode = tw.nextNode();
-    //                     var ogData = currentNode.data;
-    //                     currentNode.data = '';
-    //                     tag = callback(ogData);
-    //                     newNextNode.parentNode.insertBefore(tag, newNextNode);
-    //                     changeTWNextNode();
-    //                     currentNode = tw.currentNode;
-    //                 }
-    //             }
-    //             regex.lastIndex = 0;
-    //             str = '';
-    //         }
-    //     }
-    //     return root;
-    // }
