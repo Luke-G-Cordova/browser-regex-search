fbff66e0af5d10ef1da108c915cf519107c2fc13
Luke Cordova <lukethegreat000@gmail.com>
2021-09-01 16:30:26 -0700

    switching to a dom based popup rather than googles provided popup method

--- background.js
+++ background.js
@@ -16,9 +16,11 @@ chrome.runtime.onConnect.addListener((port) => {
 chrome.commands.onCommand.addListener((command) => {
     if(command === 'open_popup'){
         let sendObj = {
-            
+            from: 'background',
+            subject: command
         };
         sendData(sendObj);
+        console.log('back');
     }
 });
 chrome.runtime.onMessage.addListener((msg, sender, response) => {
