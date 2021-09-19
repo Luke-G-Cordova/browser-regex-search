e10e4dc304844af9bac8df88a620bc9d2e07ce2b
Luke Cordova <lukethegreat000@gmail.com>
2021-09-19 16:37:06 -0700

    using inset box shadow to make the top and bottom border instead of the top and bottom css border keys

--- popupTpose.js
+++ popupTpose.js
@@ -1,26 +1,27 @@
 
 
 
-
 function addHighlights(elem, prefix){
     // default
     prefix || (prefix = elem.tagName.toLowerCase());
     
-    if(elem.style.display === '') elem.style.display = 'block';
+    if(elem.style.display === '') elem.style.display = 'flex';
     if(elem.style.position === '') elem.style.position = 'relative';
     if(elem.style.borderRadius === '') elem.style.borderRadius = '7px';
 
-
-    let topH = document.createElement(`${prefix}-highlights`);
-    topH.className = `${prefix}-highlights`;
-    topH.style.display = 'flex';
-    topH.style.justifyContent = 'center';
-    topH.style.width = 'inherit';
-    topH.style.height = 'inherit';
-    topH.style.borderRadius = 'inherit';
-    topH.style.boxSizing = 'border-box';
-    topH.style.borderTop = '3px solid rgba(255, 255, 255, .6)';
-    topH.style.borderBottom = '3px solid rgba(0, 0, 0, .3)';
+    elem.style.boxShadow = 
+        `inset 0 4px 0 rgba(255,255,255,.5), inset 0 -4px 0 rgba(0,0,0,.3)`;
+    elem.style.justifyContent = 'center';
+    // let topH = document.createElement(`${prefix}-highlights`);
+    // topH.className = `${prefix}-highlights`;
+    // topH.style.display = 'flex';
+    // topH.style.justifyContent = 'center';
+    // topH.style.width = 'inherit';
+    // topH.style.height = 'inherit';
+    // topH.style.borderRadius = 'inherit';
+    // topH.style.boxSizing = 'border-box';
+    // topH.style.borderTop = '3px solid rgba(255, 255, 255, .6)';
+    // topH.style.borderBottom = '3px solid rgba(0, 0, 0, .3)';
     
 
 
@@ -28,21 +29,18 @@ function addHighlights(elem, prefix){
     bubble.className = `${prefix}-bubble`;
     bubble.style.display = 'block';
     bubble.style.borderRadius = 'inherit';
-    bubble.style.width = elem.clientWidth - 4 + 'px';
-    
+    bubble.style.width = elem.clientWidth - 8 + 'px';
 
     let hOffset = elem.clientHeight / elem.clientWidth;
     
-    console.log(hOffset);
     bubble.style.height = (elem.clientHeight/(hOffset + 3)) * hOffset + 'px';
-    bubble.style.marginTop = '2px';
+    bubble.style.marginTop = '7px';
     bubble.style.backgroundColor = 'rgba(255, 255, 255, .3)';
-    bubble.style.boxShadow = 'inset 0 0 10px rgba(255, 255, 255, .4)';
+    bubble.style.boxShadow = `inset 0 0 10px rgba(255, 255, 255, .5)`;
     
+    elem.appendChild(bubble);
 
-    topH.appendChild(bubble);
-
-    elem.appendChild(topH);
+    // elem.appendChild(topH);
 
     return elem;
 }
