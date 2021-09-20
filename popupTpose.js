c9b65a8e96c31b92eb7c8c658790f5b7c1e067e9
Luke Cordova <lukethegreat000@gmail.com>
2021-09-19 17:16:34 -0700

    doing the bubble style with box shadows as well now

--- popupTpose.js
+++ popupTpose.js
@@ -8,37 +8,37 @@ function addHighlights(elem, prefix){
     if(elem.style.display === '') elem.style.display = 'flex';
     if(elem.style.position === '') elem.style.position = 'relative';
     if(elem.style.borderRadius === '') elem.style.borderRadius = '7px';
+    let elemBackgroundColor = window.getComputedStyle(elem, null).getPropertyValue('background-color');
+
+
+    
+
+
+
 
     elem.style.boxShadow = 
-        `inset 0 4px 0 rgba(255,255,255,.5), inset 0 -4px 0 rgba(0,0,0,.3)`;
+        `inset 0 4px 0 rgba(255,255,255,.5), 
+        inset 0 -4px 0 rgba(0,0,0,.3),
+        inset 5px 8px 0 ${elemBackgroundColor}, 
+        inset -5px 8px 0 ${elemBackgroundColor}, 
+        inset -5px -${elem.clientHeight - (elem.clientHeight * elem.clientHeight/elem.clientWidth)}px 0 ${elemBackgroundColor}, 
+        inset 5px -${elem.clientHeight - (elem.clientHeight * elem.clientHeight/elem.clientWidth)}px 0 ${elemBackgroundColor}, 
+        inset 10px 10px 0 ${elem.clientHeight / 2}px rgba(255,255,255,.5)`;
     elem.style.justifyContent = 'center';
-    // let topH = document.createElement(`${prefix}-highlights`);
-    // topH.className = `${prefix}-highlights`;
-    // topH.style.display = 'flex';
-    // topH.style.justifyContent = 'center';
-    // topH.style.width = 'inherit';
-    // topH.style.height = 'inherit';
-    // topH.style.borderRadius = 'inherit';
-    // topH.style.boxSizing = 'border-box';
-    // topH.style.borderTop = '3px solid rgba(255, 255, 255, .6)';
-    // topH.style.borderBottom = '3px solid rgba(0, 0, 0, .3)';
-    
 
+    // let bubble = document.createElement(`${prefix}-bubble`);
+    // bubble.className = `${prefix}-bubble`;
+    // bubble.style.display = 'block';
+    // bubble.style.borderRadius = 'inherit';
+    // bubble.style.width = elem.clientWidth - 8 + 'px';
 
-    let bubble = document.createElement(`${prefix}-bubble`);
-    bubble.className = `${prefix}-bubble`;
-    bubble.style.display = 'block';
-    bubble.style.borderRadius = 'inherit';
-    bubble.style.width = elem.clientWidth - 8 + 'px';
+    
+    // bubble.style.height = (elem.clientHeight/(hOffset + 3)) * hOffset + 'px';
+    // bubble.style.marginTop = '7px';
+    // bubble.style.backgroundColor = 'rgba(255, 255, 255, .3)';
+    // bubble.style.boxShadow = `inset 0 0 10px rgba(255, 255, 255, .5)`;
 
-    let hOffset = elem.clientHeight / elem.clientWidth;
-    
-    bubble.style.height = (elem.clientHeight/(hOffset + 3)) * hOffset + 'px';
-    bubble.style.marginTop = '7px';
-    bubble.style.backgroundColor = 'rgba(255, 255, 255, .3)';
-    bubble.style.boxShadow = `inset 0 0 10px rgba(255, 255, 255, .5)`;
-    
-    elem.appendChild(bubble);
+    // elem.appendChild(bubble);
 
     // elem.appendChild(topH);
 
