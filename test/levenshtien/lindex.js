a66d3977c8ac2458a73e4bdf7b98c9e3d70ac929
Luke Cordova <lukethegreat000@gmail.com>
2022-04-19 18:46:46 -0700

    levenshtien algorithm to find the closest fitting string within a larger string seems to be working

--- test/levenshtien/lindex.js
+++ test/levenshtien/lindex.js
@@ -19,13 +19,51 @@ function lev_distance(str1, str2){
             }
         }
     }
-    return mat[mat.length - 1][mat[mat.length - 1].length - 1];
+    let dis = mat[mat.length - 1][mat[mat.length - 1].length - 1];
+    return (1 - (dis/Math.max(str1.length, str2.length))) * 100;
 }
+function findClosestMatch(search, content){
+    let matches = [];
+    let percent = 0;
+    let bPercent = 0;
 
-let myStr1 = 'hillo';
-let myStr2 = 'hello';
-
-let dis = lev_distance(myStr1, myStr2);
-
-dis = (1 - (dis/Math.max(myStr1.length, myStr2.length))) * 100;
-console.log(dis + '%');
+    for(let i = 0;i + search.length - 1 < content.length;i++){
+        let curSelection = content.substring(i, i+search.length);
+        percent = lev_distance(search, curSelection);
+        if(percent >= bPercent){
+            if(percent > bPercent){
+                matches = [];
+                bPercent = percent;
+            }
+            let end = 0;
+            let nbPercent = bPercent;
+            let lastPercent = bPercent;
+            let longSelection;
+            do{
+                end++;
+                lastPercent = nbPercent;
+                nbPercent = percent;
+                longSelection = content.substring(i, i+search.length + end);
+                percent = lev_distance(search, longSelection);
+            }while(percent > nbPercent);
+            match = [];
+            match['found'] = content.substring(i, i + search.length + end - 1);
+            match['size'] = match['found'].length;
+            match['percent'] = nbPercent;
+            match['index'] = i;
+            match['length'] = 4;
+            matches.push(match);
+        }
+    }
+    matches = matches.reduce((prev, cur) => {
+        if(prev.length === 0 || cur.percent === prev[0].percent){
+            prev.push(cur);
+        }else if(cur.percent > prev[0].percent){
+            prev = [];
+            prev.push(cur);
+        }
+        return prev;
+    }, []);
+    
+    return matches;
+}
