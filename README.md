d1cd374ac615216f547869f975765ff640d7c085
Luke Cordova <lukethegreat000@gmail.com>
2022-08-17 20:45:40 -0700

    set up

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # chrome-regeggs
 
+- https://developer.chrome.com/docs/extensions/mv3/manifest/ for manifest v3 format
+
 Adding functionality to the ctrl+f search function of google Chrome
+
 <div>Additional functionalities:</div>
 <ol>
     <li>regular expression search</li>
--- src.back/background.js
+++ src.back/background.js
@@ -1,38 +1,35 @@
-
-
 chrome.commands.onCommand.addListener((command) => {
-    if(command === 'open_popup'){
-        let sendObj = {
-            from: 'background',
-            subject: command
-        };
-        sendData(sendObj);
-    }
+  if (command === "open_popup") {
+    let sendObj = {
+      from: "background",
+      subject: command,
+    };
+    sendData(sendObj);
+  }
 });
 chrome.runtime.onMessage.addListener((msg, sender, response) => {
-    if(msg.from === 'highlight'){
-        if(msg.subject === 'newTabIframe'){
-            chrome.tabs.create({url:msg.url});
-        }
+  if (msg.from === "highlight") {
+    if (msg.subject === "newTabIframe") {
+      chrome.tabs.create({ url: msg.url });
     }
+  }
 });
 chrome.action.onClicked.addListener((tab) => {
-    let sendObj = {
-        from: 'background', 
-        subject: 'open_popup'
-    }
-    sendData(sendObj);
-    
+  let sendObj = {
+    from: "background",
+    subject: "open_popup",
+  };
+  sendData(sendObj);
 });
 
-function sendData(sendObj){
-    chrome.tabs.query({
-        active: true,
-        currentWindow: true
-    }, tabs => {
-        chrome.tabs.sendMessage(
-            tabs[0].id,
-            sendObj
-        );
-    });
+function sendData(sendObj) {
+  chrome.tabs.query(
+    {
+      active: true,
+      currentWindow: true,
+    },
+    (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, sendObj);
+    }
+  );
 }
