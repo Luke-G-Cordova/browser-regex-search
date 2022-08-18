61646f91de345eaae620fdee12da53a878973ec3
Luke Cordova <lukethegreat000@gmail.com>
2022-08-18 02:23:00 -0700

    fixed more bugs with the build script

--- bsr.ps1
+++ bsr.ps1
@@ -3,12 +3,12 @@
 # chrome manifest transfer to build directory
 
 # set up file path variables
-$chromeFolder = ".\browser-search-regex\chrome\"
-$buildChromeFolder = ".\build\chrome\"
+$chromeFolder = "./browser-search-regex/chrome/"
+$buildChromeFolder = "./build/chrome/"
 $manifestFile = "manifest.json"
 
 # delete all items currently in the build folder
-Remove-Item $buildChromeFolder -Recurse -Exclude *.js, *.js.map
+Remove-Item $buildChromeFolder -Recurse 
 
 # copy all files that are not of type .ts or .json
 Get-ChildItem "$($chromeFolder)" -Exclude *.ts, *.json | 
@@ -28,21 +28,12 @@ $manifestJSON.content_scripts[0].css = New-Object -TypeName 'System.Collections.
 # loop through each .ts and .css file and add their paths to the manifest
 Get-ChildItem "$($chromeFolder)*" -Include *.ts, *.css -Exclude *background.ts -Recurse | 
   ForEach-Object { 
-    $curRoot = (Split-Path -Path $_)
-    $rightSide = (Split-Path -Leaf -Resolve $_)
-    while ($curRoot -ne $PSScriptRoot) {
-      $rightSide = "$(Split-Path -Leaf -Resolve $curRoot)\$($rightSide)"
-      $curRoot = (Split-Path -Path $curRoot)
-    }
+    $fileName = (Split-Path -Leaf -Resolve $_)
     if ((Get-ChildItem $_ | Select-Object Extension).Extension -eq ".ts") {
-      $manifestJSON.content_scripts[0].js.Add(
-        ".\$($rightSide)".replace('browser-search-regex', 'build').replace('.ts', '.js')
-      )
+      $manifestJSON.content_scripts[0].js.Add("./$($fileName)".replace('.ts', '.js'))
     }
     elseif ((Get-ChildItem $_ | Select-Object Extension).Extension -eq ".css") {
-      $manifestJSON.content_scripts[0].css.Add(
-        ".\$($rightSide)".replace('browser-search-regex', 'build')
-      )
+      $manifestJSON.content_scripts[0].css.Add("./$($fileName)")
     }
   } | 
   Out-Null 
