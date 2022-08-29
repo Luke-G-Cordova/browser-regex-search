f68be600af9f4ea4d26b1112ab0dbdff50a027f0
Luke Cordova <lukethegreat000@gmail.com>
2022-08-29 09:55:12 -0700

    save

--- browser-search-regex/chrome/domPopup.ts
+++ browser-search-regex/chrome/domPopup.ts
@@ -5,5 +5,12 @@ chrome.runtime.onMessage.addListener((msg) => {
 });
 
 const showPopup = () => {
-  console.log('hello world');
+  let popup = document.createElement('bsr-popup-card');
+  popup.id = 'bsr-popup-card-id';
+  popup.style.top = `${20 + window.scrollY}px`;
+  popup.style.left = `${20 + window.scrollX}px`;
+  popup = document.body.insertBefore(popup, document.body.firstChild);
+
+  // let popupDragger = new Draggable(popup);
+  // popupDragger.drag();
 };
