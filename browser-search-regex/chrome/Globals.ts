9c23bef208d7a7059e7cbca7abf7c1c8d2818d61
Luke Cordova <lukethegreat000@gmail.com>
2022-08-29 12:55:14 -0700

    file name changing

---
+++ browser-search-regex/chrome/Globals.ts
@@ -0,0 +1,8 @@
+namespace Globals {
+  export let CUR_INDEX = 0;
+  export let ELEM_KEYS = [];
+  export let CURRENT_INDEXES = [];
+  export let MY_HIGHLIGHTS = [];
+  export let DEF_REJECTS = ['\\', ''];
+  export let popup: HTMLElement | null;
+}
