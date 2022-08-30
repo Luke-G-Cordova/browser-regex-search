8126dde781fe5446e8cf3f873f6767fb70fce68e
Luke Cordova <lukethegreat000@gmail.com>
2022-08-29 21:08:02 -0700

    fixing types

--- browser-search-regex/chrome/controller.ts
+++ browser-search-regex/chrome/controller.ts
@@ -1,6 +1,6 @@
 /// <reference path="./custom_lib/DOM_manipulator/DomPopup.ts" />
 
-chrome.runtime.onMessage.addListener((msg) => {
+chrome.runtime.onMessage.addListener((msg: communicationInfo) => {
   if (msg.from === 'background' && msg.subject === 'open_popup') {
     DomPopup.showPopup();
   }
