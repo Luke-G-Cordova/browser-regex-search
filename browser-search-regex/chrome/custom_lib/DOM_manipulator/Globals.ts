00f9d66921877cab7ee1e4baf67179723542a916
Luke Cordova <lukethegreat000@gmail.com>
2022-10-21 14:37:44 -0700

    only 6 inputs allowed at a time

--- browser-search-regex/chrome/custom_lib/DOM_manipulator/Globals.ts
+++ browser-search-regex/chrome/custom_lib/DOM_manipulator/Globals.ts
@@ -4,6 +4,7 @@ namespace Globals {
   export let CURRENT_INDEXES: number[] = [];
   export let MY_HIGHLIGHTS: any = [];
   export let DEF_REJECTS = ['\\', ''];
+  export let INPUT_AMT: number = 0;
   export let popup: HTMLElement | null;
   export let popupDragger: any;
   export const getGI = (key: string) => ELEM_KEYS.indexOf(key);
